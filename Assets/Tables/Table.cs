4f22d1f1e9addbd117981099c1c849ad923e60e9
Jun Loke <rebecca.j.loke@gmail.com>
2021-10-14 16:53:33 -0400

    Prefab set up

--- Assets/Tables/Table.cs
+++ Assets/Tables/Table.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 
 public class Table : MonoBehaviour
 {
+    
     private bool isDirty = false;
+
+    [SerializeField] private Animator anim;
+    public GameObject bloodEffect;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        anim.SetBool("isDirty", isDirty);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        anim.ResetTrigger("justCleaned");
     }
 
     public void infect()
     {
         isDirty = true;
+        anim.SetBool("isDirty", isDirty);
+
     }
     public void cleanTable()
     {
-        Debug.Log(isDirty? "dirty!!": "clean!");
+        Debug.Log(isDirty? "Dirty!!!": "Already clean!");
+
+        if (isDirty)
+        {
+            isDirty = false;
+            anim.SetBool("isDirty", isDirty);
+            anim.SetTrigger("justCleaned");
+        }
+        
     }
 }
