a3853116399c8d7fed721f6b5af00024ddf8fe64
arejayelle <rebecca.j.loke@gmail.com>
2021-10-24 19:13:35 -0400

    SocialDistancing that pulls enemy towards you
    uses LineUpdater

---
+++ Assets/SocialDistancing.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialDistancing : MonoBehaviour
+{
+    [SerializeField] Transform firePoint;
+    [SerializeField] LineUpdater LineUpdater;
+    [SerializeField] LineRenderer linerenderer;
+    
+    [SerializeField] float pullSpeed = 4f;
+    [SerializeField] float yForce = 6f;
+    private EnemyLogic pulledEnemy;
+
+    public GameObject destroyEffect;
+    [SerializeField] private LayerMask whatToPull;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        var firePointPosition = firePoint.position;
+
+        var hitInfo = Physics2D.Raycast(firePointPosition, firePoint.right, whatToPull);
+        if (hitInfo)
+        {
+            if (hitInfo.transform.CompareTag("Enemy"))
+            {
+                pulledEnemy = hitInfo.transform.GetComponent<EnemyLogic>();
+
+                var pullRight = (pulledEnemy.transform.position.x - firePointPosition.x) > 0f;
+                var xForce = (pullRight ? -1 : 1) * pullSpeed;
+                
+                hitInfo.transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(xForce, yForce), ForceMode2D.Impulse);
+                StartCoroutine(LineUpdater.PullTarget(pulledEnemy.transform));
+            }
+            else
+            {
+                StartCoroutine(LineUpdater.PullNothing(firePointPosition + firePoint.right * 50));
+            }
+        }
+        else
+        {
+            StartCoroutine(LineUpdater.PullNothing(firePointPosition + firePoint.right * 50));
+        }
+
+    }
+}
