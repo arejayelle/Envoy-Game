d17799a12c49d74a04534bfdb3a280b0f96e2f70
arejayelle <rebecca.j.loke@gmail.com>
2021-10-25 20:57:12 -0400

    Format score

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -26,7 +26,7 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        scoreText.text = mScore.ToString();
+        scoreText.text = $"Score: {mScore}";
     }
 
     // Update is called once per frame
@@ -38,6 +38,6 @@ public class ScoreManager : MonoBehaviour
     public void GainPoint(int points)
     {
         mScore += points;
-        scoreText.text = mScore.ToString();
+        scoreText.text = $"Score: {mScore}";
     }
 }
