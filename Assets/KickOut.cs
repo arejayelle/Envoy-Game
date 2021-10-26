0bb6cd4f8c340bb3af8a3378eb01af0c8569a0cc
arejayelle <rebecca.j.loke@gmail.com>
2021-10-25 23:32:51 -0400

    KickOut only applied to infected enemies

--- Assets/KickOut.cs
+++ Assets/KickOut.cs
@@ -8,10 +8,10 @@ public class KickOut : MonoBehaviour
     [SerializeField] Transform firePoint;
 
     [SerializeField] float yeetSpeed = 10f;
-    [SerializeField] float yForce = 6f;
+    [SerializeField] float yForce = 3f;
 
-    [SerializeField] private LayerMask whatToPull;
-    
+    [SerializeField] private LayerMask whatToShoo;
+
 
     // Update is called once per frame
     void Update()
@@ -26,20 +26,27 @@ public class KickOut : MonoBehaviour
     {
         var firePointPosition = firePoint.position;
 
-        var hitInfo = Physics2D.Raycast(firePointPosition, firePoint.right,1f,whatToPull);
+        var hitInfo = Physics2D.Raycast(firePointPosition, firePoint.right, 1f, whatToShoo);
         if (hitInfo)
         {
             if (hitInfo.transform.CompareTag("Enemy"))
             {
                 var enemy = hitInfo.transform.GetComponent<EnemyLogic>();
+                if (enemy.isInfected)
+                {
+                    var fromRight = (enemy.transform.position.x - firePointPosition.x) > 0f;
 
-                var fromRight = (enemy.transform.position.x - firePointPosition.x) > 0f;
-                var xForce = (fromRight ? 1 : -1) * yeetSpeed;
-                
-                hitInfo.transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(xForce, yForce), ForceMode2D.Impulse);
-                
+                    if ((fromRight && !enemy.isMovingRight) || // they are on your right and moving left
+                        (!fromRight && enemy.isMovingRight)) // they are on your left and moving right
+                    {
+                        enemy.Flip();
+                    }
+
+                    var xForce = (fromRight ? 1 : -1) * yeetSpeed;
+                    hitInfo.transform.GetComponent<Rigidbody2D>()
+                        .AddForce(new Vector2(xForce, yForce), ForceMode2D.Impulse);
+                }
             }
         }
-
     }
 }
