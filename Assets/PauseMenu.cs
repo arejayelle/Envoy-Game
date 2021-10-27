713669a1c8664c4fbf3cf6e92cf02a401c822d23
arejayelle <rebecca.j.loke@gmail.com>
2021-10-27 15:00:18 -0400

    Add button linkage

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
@@ -10,6 +11,11 @@ public class PauseMenu : MonoBehaviour
 
     public GameObject pauseMenuUI;
 
+    
+    // controller nav https://www.youtube.com/watch?v=SXBgBmUcTe0&ab_channel=gamesplusjames
+    public GameObject pauseFirstButton, loseFirstButton;
+    
+    
     // Update is called once per frame
     void Update()
     {
@@ -29,6 +35,8 @@ public class PauseMenu : MonoBehaviour
     void Pause()
     {
         TimeManager.Instance.Pause();
+        EventSystem.current.SetSelectedGameObject(pauseFirstButton);
+        
         pauseMenuUI.SetActive(true);
     }
     
@@ -36,6 +44,7 @@ public class PauseMenu : MonoBehaviour
     {
         Debug.Log("Resume");
         TimeManager.Instance.Resume();
+        EventSystem.current.SetSelectedGameObject(null);
         pauseMenuUI.SetActive(false);
     }
 
@@ -48,6 +57,7 @@ public class PauseMenu : MonoBehaviour
     public void RePlayGame()
     {
         TimeManager.Instance.Reset();
+
         SceneManager.LoadScene("Respawn");
     }
     public void ReplaySpecial()
