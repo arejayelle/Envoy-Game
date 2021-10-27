1e998c2cb9170d142f504f38a89104da5a078aad
arejayelle <rebecca.j.loke@gmail.com>
2021-10-26 20:26:45 -0400

    Game Manager handles game end

---
+++ Assets/gameLogic/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public PlayerController player;
+    
+    public GameObject loseScreen;
+
+    [Header("text ")]
+    [SerializeField] TextMeshProUGUI RoundText;
+    // [SerializeField] TextMeshProUGUI RoundText;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public IEnumerator EndGame()
+    {
+        player.Kill();
+        PopulateLoseScreen();
+        yield return new WaitForSeconds(1f);
+        TimeManager.Pause();
+        loseScreen.SetActive(true);
+    }
+
+    private void PopulateLoseScreen()
+    {
+        var RoundsCompleted = RoundManager.Instance.RoundNumber - 1;
+        var MasksUsed = 420;
+        var TablesWiped = 0;
+
+        RoundText.text = $"Rounds Completed: {RoundsCompleted}";
+    }
+}
